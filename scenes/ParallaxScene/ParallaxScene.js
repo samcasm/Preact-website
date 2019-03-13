71d6f53320e7a75fb1bb3815484fe3dd5e1f777e
Samir Kutty <samir7kutty@gmail.com>
2019-03-13 16:43:54 -0400

    created parallax screen

--- scenes/ParallaxScene/ParallaxScene.js
+++ scenes/ParallaxScene/ParallaxScene.js
@@ -11,8 +11,12 @@ export default class ParallaxScene extends Component {
 			<section class="parallax-scene">
 				<div class="rellax js"><img src="/assets/assetJS.png" /></div>
 				<div class="rellax node"><img src="/assets/assetnodeJS.png" /></div>
-				<div class="rellax react"><img class="react" src="/assets/assetreact.png" /></div>
-				<div class="rellax mongodb"><img class="mongodb" src="/assets/mongoDB.png" /></div>
+				<div class="rellax react"><img src="/assets/assetreact.png" /></div>
+				<div class="rellax mongodb"><img src="/assets/mongoDB.png" /></div>
+				<div class="rellax swift"><img src="/assets/swift.png" /></div>
+				<div class="rellax python"><img src="/assets/python.png" /></div>
+				<div class="rellax ubuntu"><img src="/assets/ubuntu.png" /></div>
+				<div class="rellax p5"><img src="/assets/p5.png" /></div>
 				<img class="guyprogramming" src="/assets/guyprogramming.png" />
 			</section>
 		);
